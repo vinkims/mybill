b744ca936dd3505ef14c129e9a4e19a49c382af9
vinkims <vinkims@github.com>
2020-07-29 20:54:14 +0300

    Use FormInput and FormButton components

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,40 +1,45 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet, TextInput, ScrollView, Button} from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import FormInput from '../components/FormInput';
+import FormButton from '../components/FormButton';
+import globalStyles from './globalStyles';
 
 export default function LoginScreen({navigation}){
     const[username, setUsername] = useState('');
     const[password, setPassword] = useState('');
+
+    // save username and login
+    saveSession = () => {
+        console.log(username, password)
+        navigation.navigate('Timetable')
+    }
+
     return(
-        <View style={styles.container}>
+        <View style={globalStyles.container}>
             <Text style={styles.loginText}>Login</Text>
-            <TextInput 
-                placeholder = 'Username' 
-            />
-            <TextInput 
-                placeholder = 'Password' 
-                keyboardType = 'visible-password' 
-                value={username}
+            <FormInput
+                labelName = 'Username'
+                value = {username}
                 onChangeText = {(text) => setUsername(text)}
             />
-            <Button
-                title='Login'
-                onPress={() => navigation.navigate('Timetable')}
+
+            <FormInput
+                labelName = 'Password'
+                value = {password}
+                onChangeText = {(text) => setPassword(text)}
+            />
+            
+            <FormButton
+                title = 'Login'
+                onPress = {this.saveSession}
             />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        alignItems:'center',
-        paddingTop:30
-    },
     loginText:{
         fontSize:20
     },
-    textInput:{
-        //height:40,
-        borderBottomColor:'gray'
-        //borderWidth:1
-    }
 });
