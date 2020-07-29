c403b94551d7630508a1e425ff795b51edb55c28
vinkims <vinkims@github.com>
2020-07-29 20:51:48 +0300

    add globalstyles file

---
+++ src/screens/globalStyles.js
@@ -0,0 +1,10 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+const {wdth, height} = Dimensions.get('screen');
+
+export default globalStyles = StyleSheet.create({
+    container:{
+        alignItems: 'center',
+        paddingTop: 30
+    }
+}); 
