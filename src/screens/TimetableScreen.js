ede8eb5b3be2787a9cd42d95e5715dec3060ea5d
vinkims <vinkims@github.com>
2020-08-02 02:07:44 +0300

    Add database configuration

--- src/screens/TimetableScreen.js
+++ src/screens/TimetableScreen.js
@@ -1,13 +1,38 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, Button} from 'react-native';
+import {openDatabase} from 'react-native-sqlite-storage';
+import globalStyles from './globalStyles';
+import FormButton from '../components/FormButton';
+
+var db = openDatabase({name: 'MyBill.db'})
 
 export default function TimetableScreen({navigation}){
+
+    useEffect(()=>{
+        db.transaction(function (txn){
+            txn.executeSql("SELECT name FROM sqlite_master WHERE type='table' AND name='table_user' ",
+            [],
+            function(tx, res){
+                console.log('item', res.rows.length);
+                if (res.rows.length == 0){
+                    txn.executeSql('DROP TABLE IF EXISTS table_user', []);
+                    txn.executeSql('CREATE TABLE IF NOT EXISTS table_user(user_id INTEGER PRIMARY KEY AUTOINCREMENT, employee_id VARCHAR, company VARCHAR(30), rate INTEGER ,date VARCHAR, start_time VARCHAR(10), end_time VARCHAR(20) )',
+                    []);
+                };
+            } );
+        });
+    }, []);
     return(
-        <View>
-            <Text>See your timetable here</Text>
-            <Button
+        <View style = {globalStyles.container}>
+            <Text>Choose one option for your timetable:</Text>
+            <FormButton
                 title = 'Create'
-                onPress={() => navigation.navigate('CreateTimecard')}
+                onPress = {() => navigation.navigate('CreateTimecard')}
+            />
+
+            <FormButton
+                title = 'View'
+                onPress = {() => navigation.navigate('ViewTimecard')}
             />
         </View>
     );
